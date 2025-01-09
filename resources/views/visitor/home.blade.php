370d631c5dffad5ad5c867d53d553104e240b9ee
tdnsyh <102989771+tdnsyh@users.noreply.github.com>
2025-01-09 15:30:23 +0700

    Update home.blade.php

--- resources/views/visitor/home.blade.php
+++ resources/views/visitor/home.blade.php
@@ -1,9 +1,40 @@
 <x-guard>
     <x-navbar></x-navbar>
     <x-alert></x-alert>
-    <div class="container mt-3">
-        <h1>
-            Home
-        </h1>
+    <div class="container mt-5">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque architecto
+                            sapiente ex nemo.</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem non
+                            dolorem molestias velit vel cupiditate? Lorem, ipsum dolor sit amet consectetur adipisicing
+                            elit. Culpa?</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="container row mt-5">
+        <div class="col-md-4">
+            <div class="ratio ratio-16x9">
+                <img src="https://awsimages.detik.net.id/community/media/visual/2023/04/14/gambar-pemandangan-6_169.jpeg?w=1200"
+                    class="img-fluid object-fit-cover" height="50px" alt="">
+            </div>
+        </div>
     </div>
 </x-guard>
